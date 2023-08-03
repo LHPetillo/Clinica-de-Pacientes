d92131c1cc2c4cbc698733a86c8379afcb2c504c
L_petillo <130942112+LHPetillo@users.noreply.github.com>
2023-08-03 02:11:25 -0300

    Add files via upload

---
+++ JS/calculo-imc.js
@@ -0,0 +1,69 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+console.log(paciente);
+
+
+//informacoes da tabela e suas class
+for(var i = 0; i < pacientes.length ; i++){
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+
+    var pesoEhValido = validaPeso(peso);
+    var alturaEhValida = validaAltura(altura);
+
+    //valida se e falso ou nao 
+    if (!pesoEhValido) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido!";
+        paciente.classList.add("paciente-invalido");
+    }
+    //valida se e falso ou nao 
+    if(!alturaEhValida){
+        console.log("Altura inválida");
+        tdImc.textContent = "Altura inválida!";
+        alturaEhValida = false;
+    
+        paciente.classList.add("paciente-invalido");
+    }
+    
+    if ( alturaEhValida && pesoEhValido) {
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
+    }
+}
+    //calcula o IMC pode usar a funcao agora
+    function calculaImc(peso, altura){
+        var imc = 0;
+
+        imc = peso / (altura * altura);
+
+        return imc.toFixed(2);
+    }
+
+    //validando o paciente peso e altura
+    function validaPeso(peso){
+
+        if (peso >= 0 && peso <= 1000) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function validaAltura(altura) {
+
+        if (altura >= 0 && altura <= 3.0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
